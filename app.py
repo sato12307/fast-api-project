f5eb7a8981e30a221b66aa4d3e502876d0760e8d
sato12307 <fw190hk21@gmail.com>
2025-07-31 23:39:19 +0900

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import os
 from fastapi import FastAPI, Request
 from supabase import create_client, Client
 from io import BytesIO
-
+from uuid import uuid4
 app = FastAPI()
 
 SUPABASE_URL = os.environ.get("SUPABASE_URL")
@@ -13,7 +13,7 @@ supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
 async def save_text(request: Request):
     data = await request.json()
     text = data["text"]
-    filename = data["filename"]
+    filename = data.get("filename", f"mental_{uuid4().hex}.txt")
 
     file_obj = BytesIO(text.encode("utf-8"))
 
